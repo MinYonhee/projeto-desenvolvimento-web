ef99b2975803670b037d2102bd3823ed63b99dea
MinYonhee <163359267+MinYonhee@users.noreply.github.com>
2024-11-10 22:24:46 +0000

    mudança api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,29 +1,16 @@
 "use strict";
 
-const ACCESS_POINT = "https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe";
-const API_HOST = "recipe-by-api-ninjas.p.rapidapi.com";
-const API_KEY = "e106c261ddmsh416bc9013ff3ef0p1dce89jsn7aa9cbf99922";
-
 /**
- * Função fetchData para buscar dados da API.
- * @param {string} query - Parâmetros de pesquisa para a receita (por exemplo, "chicken").
+ * Função fetchData para buscar dados da TheMealDB API.
+ * @param {string} query - Palavra-chave para a pesquisa (ex: "chicken").
  * @param {function} successCallback - Função de callback para processar a resposta da API.
  */
 export const fetchData = async function (query, successCallback) {
     // Formata a URL com o parâmetro de pesquisa
-    const url = `${ACCESS_POINT}?query=${encodeURIComponent(query)}`;
-
-    // Define opções do fetch incluindo cabeçalhos necessários
-    const options = {
-        method: "GET",
-        headers: {
-            "X-RapidAPI-Key": API_KEY,
-            "X-RapidAPI-Host": API_HOST
-        }
-    };
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${encodeURIComponent(query)}`;
 
     try {
-        const response = await fetch(url, options);
+        const response = await fetch(url);
 
         if (response.ok) {
             const data = await response.json();
