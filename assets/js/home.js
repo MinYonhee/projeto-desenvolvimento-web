5cb5d5714b77b12f21ee2e93d5324c8b8d563f79
MinYonhee <163359267+MinYonhee@users.noreply.github.com>
2024-11-22 04:27:33 +0000

    bugs ajustados

--- assets/js/home.js
+++ assets/js/home.js
@@ -71,70 +71,79 @@ addEventOnElements($tabBtns, "keydown", function(e) {
 /* Work with API */
 
 const addTabContent = ($currentTabBtn, $currentTabPanel) => {
-    // Criar o contêiner para as receitas
+    // Create the container for recipes
     const $gridlist = document.createElement("div");
     $gridlist.classList.add("grid-list");
 
-    // Adicionar os esqueletos de carregamento
+    // Add loading skeletons
     $currentTabPanel.innerHTML = `
         <div class="grid-list">
-            ${$skeletonCard.repeat(12)} <!-- Exibe os esqueletos enquanto carrega -->
+            ${$skeletonCard.repeat(12)} <!-- Display skeletons while loading -->
         </div>
     `;
     
-    // Fazer a requisição para a API
+    // Make the API request
     fetchData([['mealType', $currentTabBtn.textContent.trim().toLowerCase()], ...cardQueries], function(data) {
         console.log("API Response for", $currentTabBtn.textContent.trim().toLowerCase(), data);
-        // Limpar os esqueletos e adicionar as receitas
-        $currentTabPanel.innerHTML = ""; // Limpa os esqueletos de carregamento
 
-        // Criar os cartões com as receitas
-        data.hits.forEach((item, i) => {
-            const {
-                recipe: {
-                    image,
-                    label: title,
-                    totalTime: cookingTime,
-                    uri
-                }
-            } = item;
+        // Check if data is valid before proceeding
+        if (data && Array.isArray(data.hits)) {
+            // Clear the skeletons and add the recipes
+            $currentTabPanel.innerHTML = ""; // Clear the skeletons
 
-            const  recipeId = uri.slice(uri.lastIndexOf("_") + 1);
-            const isSaved = window.localStorage.getItem(`cookio-recipe${recipeId}`);
-            
-            const $card = document.createElement("div");
-            $card.classList.add("card");
-            $card.style.animationDelay = `${100 * i}ms`; // Atraso de animação para cada cartão
+            // Create the recipe cards
+            data.hits.forEach((item, i) => {
+                const {
+                    recipe: {
+                        image,
+                        label: title,
+                        totalTime: cookingTime,
+                        uri
+                    }
+                } = item;
 
-            $card.innerHTML = `
-                <figure class="card-media img-holder">
-                    <img src="${image}" width="195" height="195" loading="lazy" alt="${title}" class="img-cover">
-                </figure>
-                <div class="card-body">
-                    <h3 class="title-small">
-                        <a href="${uri}" class="card-link">${title ?? "Untitled"}</a>
-                    </h3>
-                    <div class="meta-wrapper">
-                        <div class="meta-item">
-                            <span class="material-symbols-outlined" aria-hidden="true">schedule</span>
-                            <span class="label-medium">${getTime(cookingTime).time || "<1"} ${getTime(cookingTime).timeUnit}</span>
+                const recipeId = uri.slice(uri.lastIndexOf("_") + 1);
+                const isSaved = window.localStorage.getItem(`cookio-recipe${recipeId}`);
+                
+                const $card = document.createElement("div");
+                $card.classList.add("card");
+                $card.style.animationDelay = `${100 * i}ms`; // Animation delay for each card
+
+                $card.innerHTML = `
+                    <figure class="card-media img-holder">
+                        <img src="${image}" width="195" height="195" loading="lazy" alt="${title}" class="img-cover">
+                    </figure>
+                    <div class="card-body">
+                        <h3 class="title-small">
+                            <a href="${uri}" class="card-link">${title ?? "Untitled"}</a>
+                        </h3>
+                        <div class="meta-wrapper">
+                            <div class="meta-item">
+                                <span class="material-symbols-outlined" aria-hidden="true">schedule</span>
+                                <span class="label-medium">${getTime(cookingTime).time || "<1"} ${getTime(cookingTime).timeUnit}</span>
+                            </div>
+                            <button class="icon-btn has-state ${isSaved ? "saved" : "removed"}" aria-label="Add to saved recipes" onclick="saveRecipe(this, '${recipeId}')">
+                                <span class="material-symbols-outlined bookmark-add" aria-hidden="true">bookmark_add</span>
+                                <span class="material-symbols-outlined bookmark" aria-hidden="true">bookmark</span>
+                            </button>
                         </div>
-                        <button class="icon-btn has-state ${isSaved ? "saved" : "removed"}" aria-label="Add to saved recipes" onclick="saveRecipe(this, '${recipeId}')">
-                            <span class="material-symbols-outlined bookmark-add" aria-hidden="true">bookmark_add</span>
-                            <span class="material-symbols-outlined bookmark" aria-hidden="true">bookmark</span>
-                        </button>
                     </div>
-                </div>
+                `;
+
+                $gridlist.appendChild($card);
+            });
+
+            // Add the "Show more" button
+            $currentTabPanel.appendChild($gridlist);
+            $currentTabPanel.innerHTML += `
+                <a href="./recipes.html?mealType=${$currentTabBtn.textContent.trim().toLowerCase()}" class="btn btn-secondary label-large has-state">Show more</a>
             `;
-
-            $gridlist.appendChild($card);
-        });
-
-        // Adicionar o botão "Show more" ao final
-        $currentTabPanel.appendChild($gridlist);
-        $currentTabPanel.innerHTML += `
-            <a href="./recipes.html?mealType=${$currentTabBtn.textContent.trim().toLowerCase()}" class="btn btn-secondary label-large has-state">Show more</a>
-        `;
+        } else {
+            console.error("Data is not in the expected format:", data);
+            $currentTabPanel.innerHTML = "<p>No recipes found.</p>";
+        }
+    }).catch((error) => {
+        console.error("Error fetching data:", error);
     });
 };
 
@@ -165,51 +174,57 @@ cuisineType.forEach((cuisine, index) => {
 
     // Fetch data for the current cuisine type
     fetchData([...cardQueries, ["cuisineType", cuisine]], function(data) {
-        // Clear skeleton and add actual content
-        $sliderWrapper.innerHTML = "";
+        // Clear skeletons and add actual content
+        if (data && Array.isArray(data.hits)) {
+            $sliderWrapper.innerHTML = "";
 
-        // Loop through the fetched data and create cards
-        data.hits.forEach(item => {
-            const { recipe: { image, label: title, totalTime: cookingTime, uri } } = item;
-            const recipeId = uri.slice(uri.lastIndexOf("_") + 1);
-            const isSaved = window.localStorage.getItem(`cookio-recipe${recipeId}`);
+            // Loop through the fetched data and create cards
+            data.hits.forEach(item => {
+                const { recipe: { image, label: title, totalTime: cookingTime, uri } } = item;
+                const recipeId = uri.slice(uri.lastIndexOf("_") + 1);
+                const isSaved = window.localStorage.getItem(`cookio-recipe${recipeId}`);
 
-            const $sliderItem = document.createElement("li");
-            $sliderItem.classList.add("slider-item");
+                const $sliderItem = document.createElement("li");
+                $sliderItem.classList.add("slider-item");
 
-            $sliderItem.innerHTML = `
-                <div class="card">
-                    <figure class="card-media img-holder">
-                        <img src="${image}" width="195" height="195" loading="lazy" alt="${title}" class="img-cover">
-                    </figure>
-                    <div class="card-body">
-                        <h3 class="title-small">
-                            <a href="${uri}" class="card-link">${title ?? "Untitled"}</a>
-                        </h3>
-                        <div class="meta-wrapper">
-                            <div class="meta-item">
-                                <span class="material-symbols-outlined" aria-hidden="true">schedule</span>
-                                <span class="label-medium">${getTime(cookingTime).time || "<1"} ${getTime(cookingTime).timeUnit}</span>
+                $sliderItem.innerHTML = `
+                    <div class="card">
+                        <figure class="card-media img-holder">
+                            <img src="${image}" width="195" height="195" loading="lazy" alt="${title}" class="img-cover">
+                        </figure>
+                        <div class="card-body">
+                            <h3 class="title-small">
+                                <a href="${uri}" class="card-link">${title ?? "Untitled"}</a>
+                            </h3>
+                            <div class="meta-wrapper">
+                                <div class="meta-item">
+                                    <span class="material-symbols-outlined" aria-hidden="true">schedule</span>
+                                    <span class="label-medium">${getTime(cookingTime).time || "<1"} ${getTime(cookingTime).timeUnit}</span>
+                                </div>
+                                <button class="icon-btn has-state ${isSaved ? "saved" : "removed"}" aria-label="Add to saved recipes" onclick="saveRecipe(this, '${recipeId}')">
+                                    <span class="material-symbols-outlined bookmark-add" aria-hidden="true">bookmark_add</span>
+                                    <span class="material-symbols-outlined bookmark" aria-hidden="true">bookmark</span>
+                                </button>
                             </div>
-                            <button class="icon-btn has-state ${isSaved ? "saved" : "removed"}" aria-label="Add to saved recipes" onclick="saveRecipe(this, '${recipeId}')">
-                                <span class="material-symbols-outlined bookmark-add" aria-hidden="true">bookmark_add</span>
-                                <span class="material-symbols-outlined bookmark" aria-hidden="true">bookmark</span>
-                            </button>
                         </div>
                     </div>
-                </div>
-            `;
-            $sliderWrapper.appendChild($sliderItem);
-        });
+                `;
+                $sliderWrapper.appendChild($sliderItem);
+            });
 
-        // Add "Show more" button
-        $sliderWrapper.innerHTML += `
-            <li class="slider-item" data-slider-item>
-                <a href="./recipes.html?cuisineType=${cuisine.toLowerCase()}" class="load-more-card has-state">
-                    <span class="label-large">Show more</span>
-                    <span class="material-symbols-outlined" aria-hidden="true">navigate_next</span>
-                </a>
-            </li>
-        `;
+            // Add "Show more" button
+            $sliderWrapper.innerHTML += `
+                <li class="slider-item" data-slider-item>
+                    <a href="./recipes.html?cuisineType=${cuisine.toLowerCase()}" class="load-more-card has-state">
+                        <span class="label-large">Show more</span>
+                        <span class="material-symbols-outlined" aria-hidden="true">arrow_forward</span>
+                    </a>
+                </li>
+            `;
+        } else {
+            console.error("Error fetching data for cuisine:", cuisine);
+        }
+    }).catch((error) => {
+        console.error("Error fetching data:", error);
     });
 });
